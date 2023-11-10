2f27a5524c5305d6609c2c0838f131b1c7fa32a2
stephane-nguyen <stepdh01@gmail.com>
2023-11-10 20:23:05 +0100

    add decoding frame

--- headers/decoding.h
+++ headers/decoding.h
@@ -1,9 +1,33 @@
-// #ifndef DECODING_H
-// #define DECODING_H
+/**
+ * @file decoding.h
+ * @brief Header file containing function declarations for decoding messages.
+ */
 
-// #include <stdint.h>
+#ifndef DECODING_H
+#define DECODING_H
 
-// #include "utils.h"
+#include <stdint.h>
 
+#include "generated_code.h"
 
-// #endif
+/**
+ * @brief Decode a 1-bit message into a comodo_message_t structure.
+ *
+ * This function decodes a 1-bit message into a comodo_message_t structure,
+ * where each bit in the message corresponds to a specific field in the structure.
+ *
+ * @param message Pointer to the input message.
+ */
+void decodecomodo(const uint8_t *message);
+
+/**
+ * @brief Decode a 15-bit message into a mux_message_t structure.
+ *
+ * This function decodes a 15-bit message into a mux_message_t structure,
+ * where the message is packed into specific fields in the structure.
+ *
+ * @param message Pointer to the input message.
+ */
+void decodemux(const uint8_t *message);
+
+#endif
--- src/decoding.c
+++ src/decoding.c
@@ -1,26 +1,60 @@
-// #include <stdint.h>
+#include <stdint.h>
+#include <stdio.h>
 
-// #include "decoding.h"
-// #include "utils.h"
-// #include "generated_code.h"
+#include "encoding.h"
+#include "drv_api.h"
+#include "generated_code.h"
 
-// #define POSITION_LIGHT_MESSAGE_ID 0x01 
-// #define LOW_BEAM_MESSAGE_ID 0x02
-// #define HIGH_BEAM_LIGHT_MESSAGE_ID 0x03
-// #define RIGHT_INDICATOR_MESSAGE_ID 0x04
-// #define LEFT_INDICATOR_MESSAGE_ID 0x05
+void decodecomodo(const uint8_t *message) {
+    comodo_message_t comodo;
 
-// void encodeBCGVtoBGF1(uint8_t *frame, message_t message) {
-//     if(message.id == POSITION_LIGHT_MESSAGE_ID){
-//         frame[0] = message.id
-//         frame[1] = message.lightState; 
-//     } else{
-//         perror("Wrong light message");
-//     }
-// }
+    comodo.cmdWarning = (message[0] & (1 << 0)) ? TRUE : FALSE;
+    comodo.cmdPositionLights = (message[0] & (1 << 1)) ? TRUE : FALSE;
+    comodo.cmdLowBeam = (message[0] & (1 << 2)) ? TRUE : FALSE;
+    comodo.cmdHighBeam = (message[0] & (1 << 3)) ? TRUE : FALSE;
+    comodo.cmdRightIndicator = (message[0] & (1 << 4)) ? TRUE : FALSE;
+    comodo.cmdLeftIndicator = (message[0] & (1 << 5)) ? TRUE : FALSE;
+    comodo.cmdWipers = (message[0] & (1 << 6)) ? TRUE : FALSE;
+    comodo.cmdWasher = (message[0] & (1 << 7)) ? TRUE : FALSE;
 
-// void encodeBCGVtoBGF2(uint8_t *frame, message_t message) {
-//     frame[0] = message.id
-//     frame[1] = message.lightState; 
-// }
+    // Create a static array of serial_frame_t to hold the comodo message
+    serial_frame_t serialData[DRV_MAX_FRAMES];
+    serialData[0].frameSize = sizeof(comodo_message_t);
+    memcpy(serialData[0].frame, &comodo, sizeof(comodo_message_t));
 
+    int32_t drvFd = drv_open(); 
+    int32_t result = drv_write_ser(drvFd, serialData, 1); 
+    drv_close(drvFd); 
+
+    if (result == DRV_ERROR) {
+        perror("Error while writing the serial frame");
+    } 
+}
+
+
+
+void decodemux(const uint8_t *message) {
+    uint8_t udpFrame[DRV_UDP_100MS_FRAME_SIZE];
+    mux_message_t mux;
+
+    int32_t drvFd = drv_open();
+
+    if (drvFd == DRV_ERROR){
+        perror("Error while trying to open the driver.");
+        return 1;
+    }
+
+    while (drv_read_udp_100ms(drvFd, udpFrame) != DRV_ERROR) {
+        mux.tramNumber = message[0];
+        mux.mileage = (uint32_t)((message[1] << 24) | (message[2] << 16) | (message[3] << 8) | message[4]);
+        mux.speed = message[5];
+        mux.chassisProblem = message[6];
+        mux.motorProblem = message[7];
+        mux.batteryProblem = message[8];
+        mux.fuelLevel = message[9];
+        mux.turnsPerMinute = (uint32_t)((message[10] << 24) | (message[11] << 16) | (message[12] << 8) | message[13]);        
+        mux.crc8 = message[14];
+
+        set_mux_message(mux);
+    }
+}
